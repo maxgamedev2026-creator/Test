1605a3d78d40c43f6c35c56f64a3f613dd640f54
maxgamedev2026-creator <maxgamedev2026@gmail.com>
2026-01-06 19:48:57 +0300

    Основы программирования: введение в язык C#.
    Основы программирования: введение в язык C#.

---
+++ Assets/BoxMove.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(123);
+        Debug.Log(456);
+        Debug.Log(5 + 6);
+        Debug.Log(5 * 6);
+        Debug.Log("Hello world");
+        Debug.Log("Hello world" + 5);
+
+        Debug.
+            Log("Сумма " + 5 + " и " + 6 + " равна " + (5 + 6));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Update");
+    }
+}
