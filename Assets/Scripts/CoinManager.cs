c310412674d253d799e41b476cbf74bc2f81164a
maxgamedev2026-creator <maxgamedev2026@gmail.com>
2026-01-07 23:10:43 +0300

    UI: создание пользовательского интерфейса.
    Создание элементов интерфейса в игре: счётчика монет, кнопок и ворот, меняющих характеристики персонажа.

--- Assets/Scripts/CoinManager.cs
+++ Assets/Scripts/CoinManager.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class CoinManager : MonoBehaviour
 {
 
     [SerializeField] int _numberOfCoinsInLevel1;
+    [SerializeField] TextMeshProUGUI _text;
     
     public void AddOne() {
         _numberOfCoinsInLevel1 += 1;
+        _text.text = _numberOfCoinsInLevel1.ToString();
     }
 
 }
